82d8e0b5a86106fe6276abd55a83d1c81d394e0e
Russell James <jamesru@oregonstate.edu>
2021-03-21 18:04:01 -0800

    contains duplicate complete

---
+++ containsDuplicate.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/contains-duplicate/
+
+/**
+ *  param {number[]} nums
+ * return {boolean}
+ */
+var containsDuplicate = function (nums) {
+    // uses a set to check for duplicates
+    // push array into set, then compare the lengths
+    let myset = new Set(nums);
+    console.log(myset)
+    return myset.size !== nums.length;
+};
