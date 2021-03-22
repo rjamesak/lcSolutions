025c044e59dbaf73d441d1ea4c04afc9873897ad
Russell James <jamesru@oregonstate.edu>
2021-03-21 18:07:56 -0800

    remove console.log

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -8,6 +8,5 @@ var containsDuplicate = function (nums) {
     // uses a set to check for duplicates
     // push array into set, then compare the lengths
     let myset = new Set(nums);
-    console.log(myset)
     return myset.size !== nums.length;
 };
