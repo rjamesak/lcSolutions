82621f582ed81f2a14a12a5d727039e9584dffcc
Russell James <jamesru@oregonstate.edu>
2021-05-10 21:25:14 -0800

    edit

--- moveZeroes.js
+++ moveZeroes.js
@@ -14,13 +14,27 @@ var moveZeroes = function (nums) {
     // step through the array
     // note the pos of encountered 0, append to arr
     let zeroPos = []
-    nums.forEach((num, idx) => {
+    for (let i = 0; i < nums.length; i++) {
         // when zero found, push idx to array
-        if (num === 0) {
-            zeroPos.push(num)
+        if (nums[i] === 0) {
+            zeroPos.push(i)
+        }
+        else {
+            // when non-zero encountered, swap with lowest zero
+            // if a zero has been encountered
+            if (zeroPos.length > 0) {
+                let zeroIdx = zeroPos.shift()
+                // put cur val in zero pos
+                nums[zeroIdx] = nums[i]
+                // push new val of zero to arr
+                zeroPos.push(i)
+                // make cur idx = 0
+                nums[i] = 0
+
+            }
+
         }
 
-    });
-    // when non-zero encountered, swap with lowest zero
+    };
 
 };
